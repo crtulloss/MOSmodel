3c3b2d101c66cbc794e7afcd86a30fbb08423126
CRT <caleb_tulloss@brown.edu>
2018-11-05 16:41:03 -0500

    Fix numerical issue for VDS=0

--- MOSmodel.m
+++ MOSmodel.m
@@ -29,7 +29,11 @@ this_L = 25e-4;
 num=73;
 figure
 hold on
-for i = 1:7
+
+num_data_sets = 7;
+% for rms error calculation, assuming all weights are 1
+rms_error_vgs = zeros(num_data_sets, 1);
+for i = 1:num_data_sets
     this_VGS = data_G_25_25(num*(i-1)+1:num*i, 2);
     this_IDS = data_G_25_25(num*(i-1)+1:num*i, 4);
     
@@ -41,6 +45,14 @@ for i = 1:7
     
     plot(this_VGS, this_IDS*1e6);
     plot(this_VGS, modeled_IDS*1e6);
+    
+%     % calculate rms error
+%     difference = this_IDS-modeled_IDS;
+%     normalized_difference = difference./this_IDS;
+%     sum_sq = sum(normalized_difference.^2)
+%     this_rms_error = sqrt(sum_sq/num);
+%     
+%     rms_error_vgs(i) = this_rms_error;
 end
 
 title('I_{DS} vs. V_{GS}');
@@ -51,7 +63,11 @@ ylabel('I_{DS} (\muA)');
 num=37;
 figure
 hold on
-for i = 1:1
+
+num_data_sets = 5;
+% for rms error calculation, assuming all weights are 1
+rms_error_vds = zeros(num_data_sets, 1);
+for i = 1:num_data_sets
     this_VDS = data_D_25_25(num*(i-1)+1:num*i, 1);
     this_IDS = data_D_25_25(num*(i-1)+1:num*i, 4);
     
@@ -63,6 +79,10 @@ for i = 1:1
     
     plot(this_VDS, this_IDS*1e6);
     plot(this_VDS, modeled_IDS*1e6);
+    
+%     % calculate rms error
+%     this_rms_error = sqrt(sum(((this_IDS-modeled_IDS)./this_IDS).^2)/num);
+%     rms_error_vds(i) = this_rms_error;
 end
 
 title('I_{DS} vs. V_{DS}');
@@ -82,26 +102,27 @@ func_psi_s0 = @(psi_s0_val) VGB - parameters.VFB -...
     (psi_s0_val-2*parameters.phiF-VSB)/constants.phit)) - psi_s0_val;
 psi_s0 = fsolve(func_psi_s0, ones(size(VGB))*VSB);
 
-func_psi_sL = @(psi_sL_val) VGB - parameters.VFB -...
-    parameters.gamma*sqrt(psi_sL_val + constants.phit*exp(...
-    (psi_sL_val-2*parameters.phiF-VDB)/constants.phit)) - psi_sL_val;
-psi_sL = fsolve(func_psi_sL, ones(size(VGB))*VDB);
+% the difference psi_sL - psi_s0 can be small, so instead
+% of making that the difference between two solutions, we solve
+% for it directly and us it to calculate psi_sL
+func_delta_psi_s = @(delta_psi_s_val) -delta_psi_s_val...
+    -parameters.gamma*sqrt(delta_psi_s_val + psi_s0 +...
+    constants.phit*exp((delta_psi_s_val+psi_s0-...
+    2*parameters.phiF-VDB)/constants.phit)) +...
+    parameters.gamma*sqrt(psi_s0 + constants.phit*exp(...
+    (psi_s0-2*parameters.phiF-VSB)/constants.phit));
+delta_psi_s = fsolve(func_delta_psi_s, ones(size(VGB))*(VDB-VSB));
 
-% the difference psi_sL-psi_s0 is used often
-delta_psi_s = psi_sL - psi_s0;
+% now calculate psi_sL
+psi_sL = psi_s0 + delta_psi_s;
 
-% alpha calculation - note that this is a DIFFERENT definition
-% from the way alpha is defined in the book
-alpha = 1 + parameters.gamma*(sqrt(psi_sL) - sqrt(psi_s0))./...
-    (delta_psi_s);
-
-% calculate drain current - components "due to" drift and diffusion
-IDS1 = W/L*parameters.u*parameters.Cox * (VGB - parameters.VFB ...
-    - psi_s0 - parameters.gamma*sqrt(psi_s0) -...
-    alpha.*delta_psi_s/2) .* delta_psi_s;
-IDS2 = W/L*parameters.u*parameters.Cox*constants.phit*alpha.*delta_psi_s;
-IDS = IDS1 + IDS2;
-
+% calculate drain current
+IDS1 = W/L*parameters.u*parameters.Cox * (VGB - parameters.VFB...
+    - (psi_sL+psi_s0)/2 - parameters.gamma/2*(sqrt(psi_sL)+sqrt(psi_s0)))...
+    .*delta_psi_s;
+IDS2 = W/L*parameters.u*parameters.Cox*constants.phit*(...
+    delta_psi_s + parameters.gamma*(sqrt(psi_sL) - sqrt(psi_s0)));
+IDS = IDS1+IDS2;
 end
 
 function IDS = current_from_VDS(W, L, VGS, VDS, VSB)
@@ -118,31 +139,27 @@ func_psi_s0 = @(psi_s0_val) VGB - parameters.VFB -...
     (psi_s0_val-2*parameters.phiF-VSB)/constants.phit)) - psi_s0_val;
 psi_s0 = fsolve(func_psi_s0, VSB);
 
-% psi_sL is still a vector, because in this case VDB is a vector
-% func_psi_sL = @(psi_sL_val) VGB - parameters.VFB -...
-%     parameters.gamma*sqrt(psi_sL_val + constants.phit*exp(...
-%     (psi_sL_val-2*parameters.phiF-VDB)/constants.phit)) - psi_sL_val;
-% psi_sL = fsolve(func_psi_sL, ones(size(VDS))*max(VDS)*2);
-func_psi_sL = @(psi_sL_val) (((VGB-parameters.VFB-psi_sL_val)...
-    ./parameters.gamma).^2)...
-    -psi_sL_val - constants.phit*exp(...
-    (psi_sL_val-2*parameters.phiF-VDB)/constants.phit);
-psi_sL = fsolve(func_psi_sL, VDB);
+% the difference psi_sL - psi_s0 can be small, so instead
+% of making that the difference between two solutions, we solve
+% for it directly and us it to calculate psi_sL
+func_delta_psi_s = @(delta_psi_s_val) -delta_psi_s_val...
+    -parameters.gamma*sqrt(delta_psi_s_val + psi_s0 +...
+    constants.phit*exp((delta_psi_s_val+psi_s0-...
+    2*parameters.phiF-VDB)/constants.phit)) +...
+    parameters.gamma*sqrt(psi_s0 + constants.phit*exp(...
+    (psi_s0-2*parameters.phiF-VSB)/constants.phit));
+delta_psi_s = fsolve(func_delta_psi_s, VDB-VSB);
 
-% the difference psi_sL-psi_s0 is used often
-delta_psi_s = psi_sL - psi_s0;
+% now calculate psi_sL
+psi_sL = psi_s0 + delta_psi_s;
 
-% alpha calculation - note that this is a DIFFERENT definition
-% from the way alpha is defined in the book
-alpha = 1 + parameters.gamma*(sqrt(psi_sL) - sqrt(psi_s0))./...
-    (delta_psi_s);
-
-% calculate drain current - components "due to" drift and diffusion
+% calculate drain current
 IDS1 = W/L*parameters.u*parameters.Cox * (VGB - parameters.VFB...
-    - psi_s0 - parameters.gamma*sqrt(psi_s0) -...
-    alpha.*delta_psi_s/2) .* delta_psi_s;
-IDS2 = W/L*parameters.u*parameters.Cox*constants.phit*alpha.*delta_psi_s;
-IDS = IDS1 + IDS2;
+    - (psi_sL+psi_s0)/2 - parameters.gamma/2*(sqrt(psi_sL)+sqrt(psi_s0)))...
+    .*delta_psi_s;
+IDS2 = W/L*parameters.u*parameters.Cox*constants.phit*(...
+    delta_psi_s + parameters.gamma*(sqrt(psi_sL) - sqrt(psi_s0)));
+IDS = IDS1+IDS2;
 
 end
 
